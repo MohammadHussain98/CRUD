e18fba7ad4ce62b34b8dc4b33786cccecc2caa40
MohammadHussain98 <mohammadrhussain98@gmail.com>
2022-01-28 20:44:37 +0200

    create html && css and js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-console.log('hello world')
+const courseName = document.getElementById('courseName')
+const courseCategory = document.getElementById('courseCategory')
+const coursePrice = document.getElementById('coursePrice')
+const courseDescription = document.getElementById('courseDescription')
+
+
+const arr = []
+const createCorses = function(){
+    // console.log('done');
+
+    const courses = {
+        Cname:courseName.value,
+        Ccategory:courseCategory.value,
+        Cprice:coursePrice.value,
+        CDescription:courseDescription.value ,
+
+    }
+    // console.log(courses)
+
+arr.push(courses)
+
+}
+
